1707bced2327412e86402cadd0a5ac85cf9c0eab
Jason Grimes <jason@grimesit.com>
2014-09-10 12:21:54 -0400

    Initial commit.

---
+++ sbin/lamp-backup.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+
+#
+# A basic script for backing up a LAMP server.
+#
+# See https://github.com/jasongrimes/lamp-backup for details.
+#
+# Copyright 2014 Jason Grimes <jason@grimesit.com>
+#
+
+STARTTIME=$(date +%s)
+ERROR=0
+
+THIS_SCRIPT=`basename $0`
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR=$(readlink -f $THIS_DIR/../etc)
+DEFAULT_OUTPUT_BASE_DIR=/var/backup
+DEFAULT_DIRS="/var/www /etc/apache2 /etc/php5 /var/log"
+DEFAULT_EXCLUDE_PATTERNS="core *~"
+
+usage() {
+    echo "Usage: $THIS_SCRIPT [options]"
+    echo ""
+    echo "$THIS_SCRIPT is a simple tool for backing up files and MySQL databases."
+    echo ""
+    echo "Options:"
+    echo "  -o OUTPUT_DIR, --output-dir=OUTPUT_DIR"
+    echo "                         Base directory in which to store backups."
+    echo "                         (Default: '$DEFAULT_OUTPUT_BASE_DIR')"
+    echo "  -d DIRS, --dirs=DIRS   Directories to back up."
+    echo "                         (Default: '$DEFAULT_DIRS')"
+    echo "  -e PATTERNS, --exclude=PATTERNS"
+    echo "                         Exclude files matching these patterns."
+    echo "                         (Default: '$DEFAULT_EXCLUDE_PATTERNS')"
+    echo "  -c FILE, --mysql-conf=FILE"
+    echo "                         A MySQL config file with connection information."
+    echo "                         (Default: '$CONFIG_DIR/mysql-connection.cfg')"
+    echo "  -s FILE, -s3-conf=FILE An s3cmd config file with connection info for Amazon S3."
+    echo "  -p S3_PATH, --s3-path=S3_PATH"
+    echo "                         The Amazon S3 path to copy files to (ex. s3://my-bucket/my-folder/)."
+    echo "  --do-files             Back up and compress files. (Enabled by default.)"
+    echo "  --no-files             Don't back up and compress files."
+    echo "  --do-mysql             Back up MySQL databases. (Enabled by default.)"
+    echo "  --no-mysql             Don't back up MySQL databases."
+    echo "  --do-s3                Copy backups to Amazon S3. Enabled by default if S3 is configured."
+    echo "  --no-s3                Don't copy backups to Amazon S3."
+    echo "  --do-rotate            Rotate old backups. (Enabled by default.)"
+    echo "  --no-rotate            Don't rotate old backups."
+    echo "  -q, --quiet            Quiet"
+    echo "  -v, --verbose          Verbose"
+    echo "  --help                 Print this help screen"
+    echo ""
+    echo "For more information, see: https://github.com/jasongrimes/lamp-backup"
+    echo ""
+}
+
+redirect_output() {
+    if [ "$VERBOSE" -eq 0 ]; then
+        "$@" > /dev/null
+    else
+        "$@"
+    fi
+}
+
+# Source the config files, if any.
+CONFIG_FILE=${CONFIG_FILE:-$CONFIG_DIR/lamp-backup.conf}
+if [ -r "$CONFIG_FILE" ]; then
+    . $CONFIG_FILE
+fi
+
+#
+# Parse command line arguments
+#
+
+# We need OPTS as the `eval set --' would nuke the return value of getopt.
+OPTS=`getopt -o o:d:e:c:s:p:qv --long help,quiet,verbose,output:,dirs:,exclude:,mysql-conf:,s3-conf:,s3-path:,do-files,no-files,do-mysql,no-mysql,do-rotate,no-rotate,do-s3,no-s3 -n $THIS_SCRIPT -- "$@"`
+if [ $? != 0 ] ; then usage >&2 ; exit 1 ; fi
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        -o|--output) OUTPUT_BASE_DIR="$2"; shift 2 ;;
+        -d|--dirs) DIRS="$2"; shift 2 ;;
+        -e|--exclude) EXCLUDE_PATTERNS="$2"; shift 2 ;;
+        -c|--mysql-conf) MYSQL_CONF="$2"; shift 2 ;;
+        -s|--s3-conf) S3_CONF="$2"; shift 2 ;;
+        -p|--s3-path) S3_PATH="$2"; shift 2 ;;
+        --do-files) DO_FILES=1; shift ;;
+        --no-files) DO_FILES=0; shift ;;
+        --do-mysql) DO_MYSQL=1; shift ;;
+        --no-mysql) DO_MYSQL=0; shift ;;
+        --do-s3) DO_S3=1; shift ;;
+        --no-s3) DO_S3=0; shift ;;
+        --do-rotate) DO_ROTATE=1; shift ;;
+        --no-rotate) DO_ROTATE=0; shift ;;
+        -q|--quiet) VERBOSE=0; shift ;;
+        -v|--verbose) VERBOSE=2; shift ;;
+        --help) usage >&2; exit 0 ;;
+        --) shift ; break ;;
+    esac
+done
+
+#
+# Set defaults. Any of these could optionally be defined in the external config file (../etc/lamp-backup.conf).
+#
+TAR=${TAR:-/bin/tar}
+MYSQL_BACKUP=${MYSQL_BACKUP:-$THIS_DIR/mysql-backup.sh}
+S3CMD=${S3CMD:-/usr/bin/s3cmd}
+VERBOSE=${VERBOSE:-1}
+OUTPUT_BASE_DIR=${OUTPUT_BASE_DIR:-$DEFAULT_OUTPUT_BASE_DIR}
+DIRS=${DIRS:-$DEFAULT_DIRS}
+EXCLUDE_PATTERNS=${EXCLUDE_PATTERNS:-$DEFAULT_EXCLUDE_PATTERNS}
+DO_FILES=${DO_FILES:-1}
+DO_MYSQL=${DO_MYSQL:-1}
+DO_ROTATE=${DO_ROTATE:-1}
+if [ -z "$DO_S3" ]; then
+    if [ -n "$S3_PATH" ]; then
+        DO_S3=1;
+    else
+        DO_S3=0;
+    fi
+fi
+
+# Create output directory
+date=`date +%Y-%m-%d-%s`
+outdir=backup_$date
+fullpath=$OUTPUT_BASE_DIR/$outdir
+if [ "$VERBOSE" -ge 1 ]; then echo "Creating $fullpath"; fi
+mkdir -p $fullpath
+if [ ! -w $fullpath ]; then
+    echo "Error creating output directory '$fullpath'" >&2
+    echo "Aborting." >&2
+    exit 1
+fi
+
+# Do MySQL backup
+if [ "$DO_MYSQL" -gt 0 ]; then
+    blockstarttime=$(date +%s)
+
+    if [ "$VERBOSE" -ge 1 ]; then
+        echo "--------------------------";
+        echo "Backing up MySQL databases";
+        echo "--------------------------";
+    fi
+
+    dumpdir=$fullpath/mysqldump
+    if [ "$VERBOSE" -ge 2 ]; then echo "Creating $dumpdir..."; fi
+    mkdir -p $dumpdir
+
+    mysql_args="-o $dumpdir"
+    if [ -n "$MYSQL_CONF" ]; then mysql_args="$mysql_args -c $MYSQL_CONF"; fi
+    VERBOSE=$VERBOSE $MYSQL_BACKUP $mysql_args
+
+    if [ $? -eq 0 ]; then
+        if [ "$VERBOSE" -ge 1 ]; then echo "Compressing $dumpdir..."; fi
+        $TAR czf $dumpdir.tgz -C $fullpath mysqldump && rm -rf $dumpdir
+    else
+        ERROR=1
+    fi
+
+    if [ "$VERBOSE" -ge 1 ]; then
+        now=$(date +%s)
+        echo "Elapsed time: $((now - $blockstarttime)) seconds."
+    fi
+fi
+
+# Do file backup
+if [ "$DO_FILES" -gt 0 ]; then
+    blockstarttime=$(date +%s)
+
+    if [ "$VERBOSE" -ge 1 ]; then
+        echo "------------------";
+        echo "Backing up files";
+        echo "------------------";
+    fi
+
+    exclude_ops=""
+    for exclude in $EXCLUDE_PATTERNS; do
+        exclude_ops="$exclude_ops --exclude='$exclude'"
+    done
+
+    for dir in $DIRS; do
+        tarfile=$fullpath/$(echo $dir | tr '/' '_' | sed -r 's/^_//').tgz
+        if [ "$VERBOSE" -ge 1 ]; then echo "Backing up $dir to $tarfile"; fi
+
+        cmd="$TAR czfp $tarfile $exclude_ops -C / .${dir}"
+        if [ "$VERBOSE" -ge 2 ]; then echo "  Executing command: $cmd"; fi
+        $cmd
+        if [ "$?" -ge 1 ]; then
+            ERROR=1
+        fi
+    done
+
+    if [ "$VERBOSE" -ge 1 ]; then
+        echo "File backup complete."
+        now=$(date +%s)
+        echo "Elapsed time: $((now - $blockstarttime)) seconds."
+    fi
+fi
+
+# Copy to S3
+if [ "$DO_S3" -gt 0 ]; then
+    blockstarttime=$(date +%s)
+
+    if [ "$VERBOSE" -ge 1 ]; then
+        echo "--------------------";
+        echo "Copying to Amazon S3";
+        echo "--------------------";
+    fi
+    if [ ! -x "$S3CMD" ]; then
+        echo "Error: s3cmd not found at '$S3CMD'. Skipping copy to S3." >&2;
+    elif [ ! -r "$S3_CONF" ]; then
+        echo "Error: S3 config file '$S3_CONF' not found. Skipping copy to S3." >&2;
+    elif [ -z "$S3_PATH" ]; then
+        echo "Error: S3 path not defined." >&2;
+        echo "Specify with the -p parameter, or set the S3_PATH option in the config file." >&2
+        echo "Run '$THIS_SCRIPT --help' for details." >&2
+        echo "Skipping copy to S3." >&2
+    else
+        if [ "$VERBOSE" -ge 1 ]; then echo "Copying $fullpath to $S3_PATH..."; fi
+        cmd="$S3CMD -c $S3_CONF put --recursive $fullpath $S3_PATH"
+        if [ "$VERBOSE" -ge 2 ]; then echo "  Executing command: $cmd"; fi
+        redirect_output $cmd
+
+        if [ "$?" -ge 1 ]; then
+            ERROR=1
+        fi
+
+        if [ "$VERBOSE" -ge 1 ]; then
+            echo "Copy to S3 complete."
+            now=$(date +%s)
+            echo "Elapsed time: $((now - $blockstarttime)) seconds."
+        fi
+    fi
+fi
+
+# Rotate old backups
+if [ "$DO_ROTATE" ]; then
+    if [ "$ERROR" -ge 1 ]; then
+        echo "Skipping backup rotation because an error occurred." >&2
+    else
+        if [ "$VERBOSE" -ge 1 ]; then
+            echo "---------------------";
+            echo "Rotating old backups";
+            echo "---------------------";
+        fi
+        echo "Warning: Backup rotation is not yet supported."
+    fi
+fi
+
+# Summary
+if [ "$VERBOSE" -ge 1 ]; then
+    echo "---------------"
+    echo "Backup complete"
+    echo "---------------"
+    now=$(date +%s)
+    echo "Elapsed time: $((now - $STARTTIME)) seconds."
+    echo "Disk space used:"
+    du -sh $fullpath
+    echo "Disk space available on backup partition:"
+    df -h $OUTPUT_BASE_DIR
+fi
+
+if [ "$ERROR" -ge 1 ]; then
+    echo "---------------------" >&2
+    echo "ERROR RUNNING BACKUP" >&2
+    echo "---------------------" >&2
+    echo "The backup may have partially completed, but there was at least one error." >&2
+    echo "Check above for any error messages." >&2
+    echo "Try running with the --verbose option if you need help troubleshooting." >&2
+    exit 1
+fi
---
+++ sbin/mysql-backup.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+#
+# Back up all databases from a MySQL server with mysqldump, one database per file.
+#
+# See https://github.com/jasongrimes/lamp-backup for details.
+#
+# Copyright 2014 Jason Grimes <jason@grimesit.com>
+#
+
+THIS_SCRIPT=`basename $0`
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEFAULT_MYSQL_CONF=$(readlink -f $THIS_DIR/../etc/mysql-connection.cnf)
+DEFAULT_SKIP_DBS="information_schema performance_schema"
+
+usage() {
+    echo "Usage: $THIS_SCRIPT [options]"
+    echo ""
+    echo "Back up MySQL databases with mysqldump, one database per file."
+    echo ""
+    echo "Options:"
+    echo "  -o DIR, --output=DIR   Directory in which to save dumpfiles. (Default: '.')"
+    echo "  -c FILE, --defaults-extra-file=FILE"
+    echo "                         A MySQL config file with connection information."
+    echo "                         (Default: '$DEFAULT_MYSQL_CONF')"
+    echo "  -u USER, --user=USER   The MySQL username."
+    echo "  -p [PASSWORD], --password[=PASSWORD]"
+    echo "                         The MySQL password. A prompt is shown if PASSWORD is omitted."
+    echo "                         Warning: specifying the password on the command line is insecure (visible with 'ps', etc.)."
+    echo "                         It's recommended to specify it in the MySQL config file instead."
+    echo "  -h HOST, --host=HOST   The MySQL hostname."
+    echo "  -P PORT, --port=PORT   The MySQL port."
+    echo "  -s DBS, --skip-dbs=DBS A list of databases *not* to dump. (Default: '$DEFAULT_SKIP_DBS')"
+    echo "  -q, --quiet            Quiet."
+    echo "  -v, --verbose          Verbose."
+    echo "  -vv, --verbose=2       Very verbose."
+    echo "  --help                 Print this help screen."
+    echo ""
+    echo "For more information, see: https://github.com/jasongrimes/lamp-backup"
+    echo ""
+}
+
+# Test whether the given value is in the given array.
+# Call it like this: if [ `in_array $needle "${haystack[@]}"` ]; then ...
+function in_array() {
+  NEEDLE="$1"; shift; ARRAY=("$@")
+  for VALUE in ${ARRAY[@]}; do [ "$VALUE" == "$NEEDLE" ] && echo 1 && return; done
+}
+
+# Source the config file, if any.
+CONFIG_FILE=${CONFIG_FILE:-$(readlink -f $THIS_DIR/../etc/mysql-backup.conf)}
+if [ -r "$CONFIG_FILE" ]; then
+    . $CONFIG_FILE
+fi
+
+#
+# Parse command line arguments
+#
+
+# We need OPTS as the `eval set --' would nuke the return value of getopt.
+OPTS=`getopt -o qu:h:P:p::v::o:c: --long help,quiet,user:,host:,port:,password::,verbose::,output:,defaults-extra-file: -n $THIS_SCRIPT -- "$@"`
+if [ $? != 0 ] ; then usage >&2 ; exit 1 ; fi
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        -q|--quiet) VERBOSE=0; shift ;;
+        -v|--verbose)
+            if [ "$2" = "2" -o "$2" = "v" ]; then
+                VERBOSE=2
+            else
+                VERBOSE=1
+            fi
+            shift 2
+            ;;
+        -o|--output) OUTPUT_DIR="$2"; shift 2 ;;
+        -c|--defaults-extra-file) MYSQL_CONF="$2"; shift 2 ;;
+        -u|--user) MYSQL_USER="$2"; shift 2 ;;
+        -h|--host) MYSQL_HOST=$2; shift 2 ;;
+        -P|--port) MYSQL_PORT=$2; shift 2 ;;
+        -p|--password)
+            # p has an optional argument. As we are in quoted mode,
+            # an empty parameter will be generated if its optional
+            # argument is not found.
+            MYSQL_PASS=$2;
+            if [ -z $MYSQL_PASS ]; then
+                NEED_MYSQL_PASS="y"
+            fi
+            shift 2
+            ;;
+        -s|--skip-dbs) SKIP_DBS="$2"; shift 2 ;;
+        --help) usage >&2; exit 0 ;;
+        --) shift ; break ;;
+    esac
+done
+
+# Get mysql pass, if necessary
+if [ "$NEED_MYSQL_PASS" = "y" ]; then
+    read -s -p 'Enter mysql password: ' MYSQL_PASS
+    echo "" >&2
+fi
+
+#
+# Set defaults.
+# Any of these could optionally be defined in an external config file as well (default: ../etc/mysql-backup.conf)
+#
+MYSQL=${MYSQL:-/usr/bin/mysql}
+MYSQLADMIN=${MYSQLADMIN:-/usr/bin/mysqladmin}
+MYSQLDUMP=${MYSQLDUMP:-/usr/bin/mysqldump}
+VERBOSE=${VERBOSE:-1}
+SKIP_DBS=${SKIP_DBS:-$DEFAULT_SKIP_DBS}
+OUTPUT_DIR=${OUTPUT_DIR:-.}
+MYSQL_CONF=${MYSQL_CONF:-$DEFAULT_MYSQL_CONF}
+
+# MYSQL_HOST=${MYSQL_HOST:-localhost}
+# MYSQL_USER=${MYSQL_USER:-root}
+
+#
+# Validate arguments
+#
+if [ -z "$OUTPUT_DIR" ]; then
+    echo "Error: OUTPUT_DIR not specified." >&2
+    echo "Pass the -o option or set OUTPUT_DIR in $CONFIG_FILE." >&2
+    echo "Pass --help for details." >&2
+    exit 1;
+fi
+
+OUTPUT_DIR=$(readlink -f $OUTPUT_DIR) # Convert into an absolute path, if necessary
+if [ ! -d $OUTPUT_DIR ]; then echo "Output directory '$OUTPUT_DIR' does not exist."; exit 1; fi
+if [ ! -w $OUTPUT_DIR ]; then echo "Output directory '$OUTPUT_DIR' is not writeable."; exit 1; fi
+
+# Create MySQL connection arguments
+MYSQL_CONN_ARGS=''
+if [ -n "$MYSQL_CONF" -a -r "$MYSQL_CONF" ]; then MYSQL_CONN_ARGS="$MYSQL_CONN_ARGS --defaults-extra-file=$MYSQL_CONF"; fi
+if [ -n "$MYSQL_USER" ]; then MYSQL_CONN_ARGS="$MYSQL_CONN_ARGS -u$MYSQL_USER"; fi
+#if [ -n "$MYSQL_PASS" ]; then MYSQL_CONN_ARGS="$MYSQL_CONN_ARGS -p$MYSQL_PASS"; fi # Pass it in an environment variable instead, so it's not visible in the process list.
+if [ -n "$MYSQL_HOST" ]; then MYSQL_CONN_ARGS="$MYSQL_CONN_ARGS -h$MYSQL_HOST"; fi
+if [ -n "$MYSQL_PORT" ]; then MYSQL_CONN_ARGS="$MYSQL_CONN_ARGS -P$MYSQL_PORT"; fi
+
+# Test connecting to MySQL server
+if [ "$VERBOSE" -ge 2 ]; then echo "Testing connection to MySQL server"; fi
+MYSQL_PWD=$MYSQL_PASS $MYSQLADMIN $MYSQL_CONN_ARGS status >/dev/null
+if [ $? != 0 ] ; then echo "Aborting."; exit 1 ; fi
+
+# if [ "$VERBOSE" -ge 1 ]; then echo "Writing dump files to $OUTPUT_DIR..."; fi
+
+# Get list of dbs
+if [ "$VERBOSE" -ge 2 ]; then echo "Getting database list"; fi
+dbs=$(MYSQL_PWD=$MYSQL_PASS $MYSQL $MYSQL_CONN_ARGS --batch --skip-column-names -e 'SHOW DATABASES')
+for db in $dbs; do
+    if [ `in_array $db "${SKIP_DBS[@]}"` ]; then continue; fi
+    if [ "$VERBOSE" -ge 1 ]; then echo "Dumping database '$db' to $OUTPUT_DIR/$db.sql"; fi
+
+    #tables=$($MYSQL --batch --skip-column-names $MYSQL_CONN_ARGS -e "SHOW TABLES FROM $db")
+
+    # Determine if this database has any non-InnoDB tables.
+    # If so, we need to get a read lock.
+    non_innodb_tables=$(MYSQL_PWD=$MYSQL_PASS $MYSQL $MYSQL_CONN_ARGS --batch --skip-column-names -e "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_SCHEMA='$db' AND ENGINE != 'InnoDB'");
+    if [ ! -z "$non_innodb_tables" ]; then
+        if [ "$VERBOSE" -ge 2 ]; then echo "  Database contains non-InnoDB tables. Acquiring read lock."; fi
+        LOCK_ARGS="--lock-tables"
+    else
+        LOCK_ARGS="--single-transaction --skip-lock-tables"
+    fi
+
+    mysql_cmd="$MYSQLDUMP $MYSQL_CONN_ARGS --ignore-table=mysql.event --opt $LOCK_ARGS $db"
+    if [ "$VERBOSE" -ge 2 ]; then echo "  Executing command: $mysql_cmd >$OUTPUT_DIR/$db.sql"; fi
+    MYSQL_PWD=$MYSQL_PASS $mysql_cmd >$OUTPUT_DIR/$db.sql
+done
+
+if [ "$VERBOSE" -ge 1 ]; then echo "Database backup complete."; fi
+
